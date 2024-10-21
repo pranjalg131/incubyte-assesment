8ecc12f5996d058d0a42a5cb9145d2c157f01fea
Pranjal Gupta <pranjalg131@gmail.com>
2024-10-22 00:39:03 +0530

    feat: add support to reject negative numbers

--- src/main/java/org/example/Calculator/StringCalculator.java
+++ src/main/java/org/example/Calculator/StringCalculator.java
@@ -1,10 +1,12 @@
 package org.example.Calculator;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class StringCalculator {
 
-    public int Add(String numbers){
+    public int Add(String numbers) throws Exception {
 
         if(numbers.isEmpty()) return 0;
 
@@ -17,14 +19,27 @@ public class StringCalculator {
             delimiters = parts[0];
             numbers = parts[1];
 
-            delimiters = delimiters.replaceAll("//+", "");
+            delimiters = delimiters.replaceFirst("//", "");
 
             delimiters = "[" + delimiters + "]";
 
         }
 
-        return Arrays.stream(numbers.split(delimiters))
+        List<Integer> negatives =  new ArrayList<>();
+
+        int sum =  Arrays.stream(numbers.split(delimiters))
                 .mapToInt(Integer::parseInt)
+                .map(number -> {
+                    if(number < 0){
+                        negatives.add(number);
+                        return 0;
+                    }
+                    return number;
+                })
                 .sum();
+
+        if(!negatives.isEmpty()) throw new Exception("Negatives are not allowed. Please remove the following numbers: " + negatives.toString());
+
+        return sum;
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StringCalculatorTest {
 
     @Test
-    public void testAddWhenInputIsEmpty(){
+    public void testAddWhenInputIsEmpty() throws Exception {
 
         StringCalculator stringCalculator = new StringCalculator();
         int result = stringCalculator.Add("");
@@ -13,7 +13,7 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void testAddWhenInputHasSingleOperand(){
+    public void testAddWhenInputHasSingleOperand() throws Exception {
 
         StringCalculator stringCalculator = new StringCalculator();
         int result = stringCalculator.Add("1");
@@ -21,23 +21,31 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void testAddWhenInputHasMultipleOperand(){
+    public void testAddWhenInputHasMultipleOperand() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
         int result = stringCalculator.Add("1,2,3,4");
         assertEquals(10,result);
     }
 
     @Test
-    public void testAddWhenInputHasMultipleOperandsAndMultipleDelimiters(){
+    public void testAddWhenInputHasMultipleOperandsAndMultipleDelimiters() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
         int result = stringCalculator.Add("1\n2,3");
         assertEquals(6, result);
     }
 
     @Test
-    public void testAddWhenInputHasCustomDelimitersAndMultipleOperands(){
+    public void testAddWhenInputHasCustomDelimitersAndMultipleOperands() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
         int result = stringCalculator.Add("//;\n1;2;3");
         assertEquals(6, result);
+    }
+
+    @Test
+    public void testAddWhenInputHasNegativeNumbers(){
+        StringCalculator stringCalculator = new StringCalculator();
+        Exception exception = assertThrows(Exception.class, () -> stringCalculator.Add("//;\n-1;-2;3"));
+
+        assertEquals("Negatives are not allowed. Please remove the following numbers: [-1, -2]", exception.getMessage());
     }
 }
